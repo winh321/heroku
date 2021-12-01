6d2b97243c88534332a440d5580e8af78c6940b1
winh321 <69839416+winh321@users.noreply.github.com>
2021-12-01 13:26:28 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Win Htike</title>
+ <style>
+  marquee{
+    padding: 8px;
+    animation: animation_name 1s infinite;
+  }
+  
+  @-webkit-keyframes animation_name {
+   from {
+    background-color:red;
+    color: white
+    
+   }
+   to {
+    background-color:black;
+    color:yellow;
+   }
+  }
+  
+  @keyframes animation_name {
+   from {
+
+    color:black;
+   }
+   to {
+  
+    color:white;
+   }
+  }
+  body{background-color:white;margin: 0px;font-size:150%}
+  
+  span{border-bottom:2px solid blue}
+  </style>
+  
+  </head>
+
+<body>
+   <marquee> ယခုကဲ့သို့ ကျွန်ုပ်၏ Personal Websiteလေးဆီသို့ အလည်လာပေးသော မိတ်ဆွေများအား
+   <b>"မင်္ဂလာပါလို့" </b> နွေးထွေးစွာ ကြိုဆိုနှုတ်ခွန်းဆက်သလိုက်ပါတယ်ခင်ဗျာ...
+   </marquee>
+  <div style="text-align: center">
+    <img width="40%" src="name.png" />
+   <img width="80%" src="man.png" /> 
+   
+   <h4 onmouseout="this.style.color='black'" onmouseover="this.style.color ='red';">Android Application && Web Developer</h4>
+  
+  <blockquote style="text-align:left;border: 2px solid black;padding: 8px">
+<small>  Name : Win Htike;<br>
+  Age: 20,<br>
+  Country:Myanmar<br>
+  Work:Student,<br>
+Studied at :<br> Meiktila Education Degree College,<br>
+Live in :
+Myingyan,<br>
+Phone : +959950141131</small></blockquote>
+ </div>
+ 
+ 
+ <div style="text-align: center">
+<button style="border-radius:10px; background-color:#FF7647;color: white;width:60%;font-size:25px;padding:6px" onclick="location.href='skill.html';">My skill</button>
+ </div>
+ <br>
+ <div style="text-align: center">
+<button style="border-radius:10px; background-color:#885BFF;color: white;width:60%;font-size:20px;padding:6px" onclick="location.href='project.html';">My projects</button>
+ </div>
+ <br>
+ <div style="text-align: center">
+<button style="border-radius:10px; background-color:#C44E4D;color: white;width:60%;font-size:25px;padding:6px" onclick="location.href='donation.html';">Donation to Me </button>
+ </div>
+ 
+  <br>
+ <div style="text-align: center">
+<button style="border-radius:10px; background-color:#14C44F;color: white;width:60%;font-size:20px;padding:6px" onclick="location.href='lesson.html';">My Course and Lessons</button>
+ </div>
+ <br><br>
+ <br><br>
+<div style="text-align: center;font-size:20px;background:grey;padding: 20px">
+  <h2 style="color:white">About Contact:</h2><br>
+  <a href="fb://page/109831937947539">Facebook</a> &nbsp;&nbsp; <a href="tel:09950141131">Phone</a> &nbsp;&nbsp;<a href="mailto:iamwinhtike@gmail.com">Email</a>
+  <br><br>
+</div>
+
+</body>
+</html>
